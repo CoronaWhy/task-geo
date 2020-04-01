9c13be06e56c46161cf091e0746fb055830d26cc
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2020-04-01 11:02:43 -0500

    Fix to main.py

--- task_geo/data_sources/us_data/__main__.py
+++ task_geo/data_sources/us_data/__main__.py
@@ -5,6 +5,7 @@ Description:
     - Runs us_census which gets the finalized DataFrame for US Census Data
     - Converts and exports to CSV
 """
+from us_census import us_census
 
 def main():
 	us_census().to_csv("us-census-data.csv", header=True)
