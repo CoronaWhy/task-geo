6e3d92feffbdd963f63e28e3a81a6bb70cb29a0b
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2020-04-01 19:45:57 -0500

    Main method isort change

--- task_geo/data_sources/us_data/__main__.py
+++ task_geo/data_sources/us_data/__main__.py
@@ -5,12 +5,13 @@ Description:
     - Runs us_census which gets the finalized DataFrame for US Census Data
     - Converts and exports to CSV
 """
-from us_census import us_census     #isort:skip
+
+from us_census import us_census
 
 
 def main():
     us_census().to_csv("us-census-data.csv", header=True)
 
 
-if __name__ == '_a_min__':
+if __name__ == '__main__':
     main()
