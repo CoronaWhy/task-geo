c51e809bf81f01d5a02e3898b97ef3880d08a192
Krishna Sheth <krishna.sheth@ibm.com>
2020-04-05 01:14:55 -0400

    Add docstring to hdx api

--- task_geo/data_sources/hdx/__init__.py
+++ task_geo/data_sources/hdx/__init__.py
@@ -3,5 +3,10 @@ from task_geo.data_sources.hdx.hdx_formatter import acap_formatter
 
 
 def hdx_api():
+    """HDX Data Source
+
+    Example:
+    >>> hdx_api()
+    """
     raw = acap_connector()
     return acap_formatter(raw)
