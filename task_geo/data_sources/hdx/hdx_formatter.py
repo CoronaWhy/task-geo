46e045b0e942018664c7d0993f9d0ce045cbcbb2
Krishna Sheth <krishna.sheth@ibm.com>
2020-04-07 18:02:01 -0400

    Change ordering

--- task_geo/data_sources/hdx/hdx_formatter.py
+++ task_geo/data_sources/hdx/hdx_formatter.py
@@ -6,5 +6,9 @@ logger = logging.getLogger(__name__)
 def acap_formatter(raw):
     data = raw.copy()
     data.columns = [column.lower() for column in data.columns]
-    data = data.drop(['id', 'pcode', 'admin_level_name', 'link', 'alternative source'], axis=1)
+    data = data.drop(['pcode', 'admin_level_name', 'alternative source'], axis=1)
+    column_order = ['id', 'country', 'region', 'iso', 'category', 'measure',
+                    'targeted_pop_group', 'comments', 'non_compliance', 'date_implemented',
+                    'source', 'source_type', 'entry_date', 'link']
+    data = data[column_order]
     return data
